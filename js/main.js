003512ef037dba8f15197995d8a4f0cd81fdad3d
Viacheslav Demchenko <demchenko_slava_@mail.ru>
2018-08-28 20:31:30 +0300

    Finished main task of the twelfth lesson

---
+++ js/main.js
@@ -0,0 +1,562 @@
+window.addEventListener('DOMContentLoaded', () => {
+	
+/* ПЕРЕКЛЮЧЕНИЕ ТАБОВ */
+
+	let tab = document.getElementsByClassName('info-header-tab'),
+		tabContent = document.getElementsByClassName('info-tabcontent'),
+		info = document.getElementsByClassName('info-header')[0];
+
+
+		function hideTabContent(a) {
+			for (i = a; i < tabContent.length; i++) {
+				tabContent[i].classList.remove('show');
+				tabContent[i].classList.add('hide');
+			}
+		};
+
+		hideTabContent(1);
+
+		function showTabcontent(b) {
+			if (tabContent[b].classList.contains('hide')) {
+				hideTabContent(0);
+				tabContent[b].classList.remove('hide');
+				tabContent[b].classList.add('show');
+			}
+		};
+
+		info.addEventListener('click', (e) => {
+			let target = e.target;
+			if (target.className === 'info-header-tab') {
+				for (let i = 0; i < tab.length; i++) {
+					if (target === tab[i]) {
+						showTabcontent(i);
+						break;
+					}
+				}
+			}
+		});
+
+/* ТАЙМЕР ОБРАТНОГО ОТСЧЕТА */
+
+let deadline = '2018-08-27 23:59:59';//Задаем конечную дату
+
+//Функция расчета оставшегося времени
+function getTimeRemaining(endtime) {
+//Установка текущей даты и даты окончания акции	
+  let 	t = Date.parse(endtime) - Date.parse(new Date()),
+		seconds = Math.floor( (t / 1000) % 60),
+		minutes = Math.floor( (t / 1000 / 60) % 60),
+		hours = Math.floor( t / (1000 * 60 * 60) );
+
+  	//Возврат объекта данных времени
+  	return {
+	    'total': t,
+	    'hours': hours,
+	    'minutes': minutes,
+	    'seconds': seconds
+  	};
+};
+
+//Функция запуска таймера 
+function initializeClock(id, endtime) {
+//Задание элементов для вывода данных
+  let clock = document.getElementById(id),
+		hours = clock.querySelector('.hours'),
+		minutes = clock.querySelector('.minutes'),
+		seconds = clock.querySelector('.seconds'),
+		//Установка интервала работы таймера в 1 секунду
+		timeInterval = setInterval(updateClock, 1000);
+
+//Функция таймера обратного отсчета		
+  function updateClock() {
+    let t = getTimeRemaining(endtime);
+
+//Задаем функцию для добавления 0 к числам до 9 (01, 02 и т.д.)
+			function addZero(num){
+			if(num <= 9) {
+				return `0${num}`;
+			} else return num;
+		};
+
+//Вывод оставшегося времени 
+    hours.textContent = addZero(t.hours);
+    minutes.textContent = addZero(t.minutes);
+    seconds.textContent = addZero(t.seconds);
+
+//Действия, выполняющиеся после завершения отсчета таймера
+    if (t.total <= 0) {
+	    clearInterval(timeInterval);
+	    hours.textContent = '00';
+	    minutes.textContent = '00';
+	    seconds.textContent = '00';
+    } 
+};
+	updateClock();
+};
+	initializeClock('timer', deadline);//Запуск таймера по id
+
+/* ПЛАВНАЯ ПРОКРУТКА */
+
+//Передача в переменную всех элементов html на странице
+let elements = document.documentElement,
+	body = document.body,//Передаем в переменную body
+	links = document.links;//Получаем все якорные ссылки на странице
+
+//Функция опредления нажатой ссылки и расчета перемещения
+function calcScroll() {
+
+//Перебор циклом все ссылок и определение той, на которой был сделан клик
+  for (let i = 0; i < links.length; i++) {
+    links[i].onclick = function(event = window.event) {
+    	// event = event || window.event;//Кросс-браузерность
+      //Определение и округление текущего расстояния от верха документа
+      let scrollTop = Math.round(body.scrollTop || elements.scrollTop);
+      if (this.hash !== '') {
+//Предотвращение действия браузера по дефолту при отсутвии атрибута hash у элемента
+        event.preventDefault();
+//Получение элемента, к которому ведет якорь нажатой ссылки
+        let targetElement = document.getElementById(this.hash.substring(1)),
+//Задел в 80px, чтобы при прокрутке меню не закрывало заголовок секции
+			targetElementTop = -80;
+//Вычисление через цикл расстояния от верха до элемента, к которому ведет нажатая ссылка
+        while (targetElement.offsetParent) {
+          targetElementTop += targetElement.offsetTop;
+          targetElement = targetElement.offsetParent;
+        }
+        //Получение округленного значения расположения элемента
+        targetElementTop = Math.round(targetElementTop);
+/* Функция запуска плавного перемещения (содержит аргументы: текущее растояние от верха
+документа, расстояние от верха документа к контентному блоку, к которому ведет нажатая 
+ссылка и сам контентный блок) */
+		if (document.body.style.overflow !== 'hidden') {//Предотвращает прокрутку при открытом модальном окне
+			clearInterval(move);
+			smoothScroll(scrollTop, targetElementTop, this.hash);
+		}
+      }
+    };
+  }
+};
+calcScroll();
+
+let timeInterval = 1, //Задаем временной интервал в 1 миллисекунду
+		prevScrollTop,
+		speed,
+		move;
+//Функция плавной прокрутки
+function smoothScroll(from, to, hash) {
+/* Если элемент (конечная точка движения) расположен ниже текущей точки экрана,
+то scroll ведется с верху вниз (положительное значение), если наоборот, то снизу
+вверх (отрицательное значение) */
+	if (to > from) {
+		speed = 10;
+	} else {
+		speed = -10;
+	}
+//Установка интервала движения
+	move = setInterval(function() {
+//Получение и округение текущей позиции экрана
+    scrollTop = Math.round(body.scrollTop || elements.scrollTop);
+//Условия прекращения или продолжения движения
+    if (prevScrollTop === scrollTop || (to > from && scrollTop >= to) || (to < from && scrollTop <= to)) {
+    	clearInterval(move);
+//Добавление атрибута hash в url после прокрутки (добавляется к адресной строке в браузере)
+      history.pushState(history.state, document.title, location.href.replace(/#.*$/g, '') + hash);
+    } else {
+      body.scrollTop += speed;
+      elements.scrollTop += speed;
+/* Передача текущей позиции экрана в переменную, которая при последующих перемещениях
+будет играть роль места хранения последней позиции экрана */
+      prevScrollTop = scrollTop;
+    }
+  }, timeInterval);//Передача ранее установленного интервала перемещения
+}
+
+/* ОПРЕДЕЛЕНИЕ ТИПА БРАУЗЕРА */
+
+//Получение версии IE или Edge
+let version = detectIE(),//Задаем функцию определения IE или EDGE в переменную
+	browser;
+//Присваиваем переменной true или false в зависимости от определения браузера
+if (version === false) {
+  browser = true;
+} else if (version >= 12) {
+  browser = false;
+} else {
+  browser = false;
+}
+
+//Функция определения браузера возвращает версию IE или афдыу если браузер другой
+function detectIE() {
+//Определяем, является ли текущий браузер IE
+  let ua = window.navigator.userAgent,
+		msie = ua.indexOf('MSIE ');
+
+  if (msie > 0) {
+//Если IE 10 или младшей версии функция возвращает номер версии
+    return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+  }
+
+  let trident = ua.indexOf('Trident/');
+  if (trident > 0) {
+//Если IE 11 версии функция возвращает номер версии
+    let rv = ua.indexOf('rv:');
+    return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+  }
+
+  let edge = ua.indexOf('Edge/');
+  if (edge > 0) {
+//Усли Edge (или IE 12 версии и выше) функция возвращает номер версии
+    return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+  }
+
+//Функция возвращает false,если текущий браузер не IE и не EDGE
+  return false;
+};
+
+//Функция проверки на мобильный браузер
+let mobileBrowser;
+
+function mobileBrowserCheck() {
+//Проверка на конкретный мобильный браузер
+		let isMobile = {
+	    Android: function() {
+	        return navigator.userAgent.match(/Android/i);
+	    },
+	    BlackBerry: function() {
+	        return navigator.userAgent.match(/BlackBerry/i);
+	    },
+	    iOS: function() {
+	        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+	    },
+	    Opera: function() {
+	        return navigator.userAgent.match(/Opera Mini/i);
+	    },
+	    Windows: function() {
+	        return navigator.userAgent.match(/IEMobile/i);
+	    },
+	    //Проверка на любой мобильный браузер
+	    any: function() {
+	        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+	    }
+	};
+//Возврат функцией true, если браузер мобильный
+		if(isMobile.any()){
+			mobileBrowser = true;
+			console.log('Мобильный');
+		} else {
+			console.log('Не мобильный');
+		}
+};
+mobileBrowserCheck();
+
+/* МОДАЛЬНОЕ ОКНО */
+
+//Задаем переменные кнопки открытия модального окна, оверлея и закрытия
+let more = document.querySelector('.more'),
+	overlay = document.querySelector('.overlay'),
+	close = document.querySelector('.popup-close');
+
+//Открытие модального окна
+more.addEventListener('click', () => {
+	if (mobileBrowser !== true) {
+		more.classList.add('more-splash');
+		overlay.style.display = 'block';
+		document.body.style.overflow = 'hidden';
+		console.log('Скрипт работает');
+	} else {
+		return false;
+		console.log('Скрипт не работает');
+	}
+});
+
+//Закрытие модального окна
+close.addEventListener('click', () => {
+	if (mobileBrowser !== true) {
+		overlay.style.display = 'none';
+		more.classList.remove('more-splash');
+		document.body.style.overflow = '';
+		console.log('Скрипт работает');
+	} else {
+		console.log('Скрипт не работает');
+		return false;
+	}
+});
+
+//Закрытие модального окна нажатем Esc
+document.onkeydown = (e) => {
+	if (e.keyCode === 27) {
+		overlay.style.display = 'none';
+		more.classList.remove('more-splash');
+		document.body.style.overflow = '';	        
+    }
+};
+
+/* ПРИВЯЗКА МОДАЛЬНОГО ОКНА К КНОПКАМ В ТАБАХ */
+
+//Функция реализации обработчика события
+function descBtnFunc() {
+//Получение псевдомассива кнопок
+let descBtns = document.querySelectorAll('.description-btn');
+	if (mobileBrowser !== true) {
+//Перебор циклом всех кнопок в псевдомассиве
+		for (let i = 0; i < descBtns.length; i++) {
+//Привязка к кнопкам обработчика события
+			descBtns[i].addEventListener('click', () => {
+				descBtns[i].classList.add('more-splash');
+				overlay.style.display = 'block';
+				document.body.style.overflow = 'hidden';
+				console.log('Скрипт работает');
+			});
+		}
+	} else {
+		console.log('Скрипт не работает');
+		return false;
+	}
+};
+descBtnFunc();
+
+/* ОТПРАВКА ДАННЫХ С ФОРМЫ */
+
+//Форма
+let message = new Object();
+
+let form = document.getElementsByClassName('main-form')[0],
+	input = form.getElementsByTagName('input'),
+	statusMessage = document.createElement('div');
+
+	form.addEventListener('submit', function(event) {
+		event.preventDefault();
+		form.appendChild(statusMessage);
+
+		// AJAX
+		let request = new XMLHttpRequest();
+			request.open("POST", 'server.php');
+			request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+			let formData = new FormData(form);
+				request.send(formData);
+
+			request.onreadystatechange = function() {
+			  	if (request.readyState < 4) {
+			    //Добавляем класс с картинкой при ожидании отправки формы
+			   		statusMessage.classList.add('form-awaiting');
+				  	} else if (request.readyState === 4) {
+//Убираем класс с картинкой ожидания после достижения следующего статуса соединения с сервером
+				    	statusMessage.classList.remove('form-awaiting');
+					    if (request.status == 200) {
+					    //Добавляем класс с картинкой при успешной отправке формы
+					      	statusMessage.classList.add('form-success');               
+					    }
+						    else {
+						//Добавляем класс с картинкой при неудачной попытке отправки формы
+						      	statusMessage.classList.add('form-failure');
+						    }
+			  		}
+			};
+			//Очистка инпутов
+			for (let i = 0; i < input.length; i++) {
+				input[i].value = '';
+			}	
+	});
+
+//Оправка данных с формы в разделе "Контакты"
+let contactForm = document.getElementById('form'),
+	contactFormInput = contactForm.getElementsByTagName('input');
+
+	contactForm.addEventListener('submit', function(event) {
+		event.preventDefault();
+		contactForm.appendChild(statusMessage);
+
+	// AJAX
+	let request = new XMLHttpRequest();
+			request.open("POST", 'server.php');
+			request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+	let contactFormData = new FormData(contactForm);
+		request.send(contactForm);	
+
+		request.onreadystatechange = function() {
+			if (request.readyState < 4) {
+			//Добавляем класс с картинкой при ожидании отправки формы
+				statusMessage.classList.add('form-awaiting__contact-form');
+				console.log('Ожидание');
+			} else if (request.readyState === 4) {
+//Убираем класс с картинкой ожидания после достижения следующего статуса соединения с сервером
+					statusMessage.classList.remove('form-awaiting__contact-form');
+				if (request.status == 200 && request.status < 300) {
+					//Добавляем класс с картинкой при успешной отправке формы
+					statusMessage.classList.add('form-success__contact-form');
+					console.log('Успех');	
+				}
+				else {
+				//Добавляем класс с картинкой при неудачной попытке отправки формы
+				statusMessage.classList.add('form-failure__contact-form');
+				console.log('Ошибка');
+				}
+			}
+		};
+		//Очистка инпутов
+		for (let i = 0; i < contactFormInput.length; i++) {
+			contactFormInput[i].value = '';
+		}
+	});
+
+/* СЛАЙДЕР */
+
+let slideIndex = 1,//Устанавливаем текущий слайд
+//Получаем псевдомассив слайдов
+	slides = document.getElementsByClassName('slider-item'),
+	prev = document.querySelector('.prev'),//Стрелка влево
+	next = document.querySelector('.next'),//Стрелка вправо
+	dotsWrap = document.querySelector('.slider-dots'),//Блок точек
+	dots = document.getElementsByClassName('dot');//Псевдомассив точек
+
+showSlides(slideIndex);
+
+function showSlides(n) {
+	if (n > slides.length) {
+		slideIndex = 1;
+	};
+	if (n < 1) {
+		slideIndex = slides.length;
+	};
+
+	for (let i = 0; i < slides.length; i++) {
+		slides[i].style.display = 'none';
+	};
+
+	for (let i = 0; i < dots.length; i++) {
+		dots[i].classList.remove('dot-active');
+	};
+
+	slides[slideIndex - 1].style.display = 'block';
+	dots[slideIndex - 1].classList.add('dot-active');
+
+};
+
+	function plusSlides(n) {
+		showSlides(slideIndex += n);
+	};
+
+	function currentSlide(n) {
+		showSlides(slideIndex = n);
+	};
+
+	prev.addEventListener('click', function() {
+		plusSlides(-1);
+	});
+
+	next.addEventListener('click', function() {
+		plusSlides(1);
+	});
+
+	dotsWrap.addEventListener('click', function(e) {
+		for (let i = 0; i < dots.length + 1; i++) {
+			if (e.target.classList.contains('dot') && e.target == dots[i - 1]) {
+				currentSlide(i);
+			}
+		}
+	});
+
+/* КАЛЬКУЛЯТОР */
+
+let persons = document.getElementsByClassName('counter-block-input')[0],
+	restDays = document.getElementsByClassName('counter-block-input')[1],
+	place = document.getElementById('select'),
+	totalValue = document.getElementById('total'),
+	personsSum = 0,
+	daysSum = 0,
+	total = 0,
+	a = 0,
+	result = 0;
+
+	totalValue.innerHTML = 0;
+
+	persons.addEventListener('keyup', function() {
+//Проверка инпута на запрет ввода ё, точки, запятой, букв и спецсимволов
+		this.value = this.value.replace(/[^\d]*/g)
+                        	   .replace(/^[^\d]*(\d+([.,]\d{0,5})?).*$/g, '$1');
+// this.value = this.value.replace(/[+\.\,ёЁ]/,'');
+		personsSum = +this.value;//Присвоение переменной значения инпута
+//Обнуление итогового числа, если одно из полей пустое
+		if (restDays.value == '' || persons.value == '') {
+			totalValue.innerHTML = 0;
+			total = 0;
+//Обнуление итогового числа, если в одно из полей введен 0
+		} else if (restDays.value == 0 || persons.value == 0) {
+			totalValue.innerHTML = 0;
+			total = 0;
+		} else {
+			total = (daysSum + personsSum) * 4000;//Вычисление итоговой суммы
+//Вычисление итоговой суммы с учетом коэффициента выбранного направления путеществия
+			result = total * place.options[place.selectedIndex].value;
+			animateValue('total', 0, result, 2000);//Запуск функции анимации числа
+			// totalValue.innerHTML = total;
+		}
+	});
+
+	restDays.addEventListener('keyup', function() {
+//Проверка инпута на запрет ввода ё, точки, запятой, букв и спецсимволов
+		this.value = this.value.replace(/[^\d]*/g)
+                        	   .replace(/^[^\d]*(\d+([.,]\d{0,5})?).*$/g, '$1');
+        // this.value = this.value.replace(/[+\.\,ёЁ]/,'');
+		daysSum = +this.value;//Присвоение переменной значения инпута
+//Обнуление итогового числа, если одно из полей пустое		
+		if (persons.value == '' || restDays.value == '') {
+			totalValue.innerHTML = 0;
+			total = 0;
+//Обнуление итогового числа, если в одно из полей введен 0
+		} else if (restDays.value == 0 || persons.value == 0) {
+			totalValue.innerHTML = 0;
+			total = 0;
+		} else {
+			total = (daysSum + personsSum) * 4000;//Вычисление итоговой суммы
+//Вычисление итоговой суммы с учетом коэффициента выбранного направления путеществия	
+			result = total * place.options[place.selectedIndex].value;
+			// totalValue.innerHTML = total;
+			animateValue('total', 0, result, 2000);//Запуск функции анимации числа
+		}
+	});
+//Обнуление итогового числа, если одно из полей пустое
+	place.addEventListener('change', function() {
+		if (restDays.value == '' || persons.value == '') {
+			totalValue.innerHTML = 0;
+			total = 0;
+		} else {
+			// a = total;
+//Вычисление итоговой суммы с учетом коэффициента выбранного направления путеществия
+			result = total * place.options[place.selectedIndex].value;
+			animateValue('total', 0, result, 2000);//Запуск функции анимации числа
+			// totalValue.innerHTML = a * this.options[this.selectedIndex].value;	
+		}
+	});
+
+/* ПЕРЕБОР ЦИФР */
+
+/* Функция принимает id элемента вывода результата, начало и конец перебора цифр
+и длительность анимации */
+function animateValue(id, start, end, duration) {
+    let range = end - start,//Период перебора цифр
+        current = start,//Текущая точка отсчета
+        increment = 1000,//Шаг с которым будет увеличиваться итоговая сумма
+        //Расчет скорости перебора, которая зависит от длительности и конечного числа
+        stepTime = Math.abs(Math.floor(duration / range)),
+        element = document.getElementById(id);//Элемент вывода числа на экран
+
+//Функция таймера
+    let timer = setInterval(function() {
+        current += increment;//Увеличение текущего значения на заданный шаг
+        element.innerHTML = current;//Вывод текущего числа в элемент на экран
+        if (current === end) {//Обнуление таймера при достижении конечного числа
+            clearInterval(timer);
+        }
+    }, stepTime);
+}
+
+
+
+
+});
+
+
+
