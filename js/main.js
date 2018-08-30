e0d6700f5174c7b204659dba8b402630f2c24bd3
Viacheslav Demchenko <demchenko_slava_@mail.ru>
2018-08-30 11:34:19 +0300

    Corrected mistakes

--- js/main.js
+++ js/main.js
@@ -97,79 +97,37 @@ function initializeClock(id, endtime) {
 
 /* ПЛАВНАЯ ПРОКРУТКА */
 
-//Передача в переменную всех элементов html на странице
-let elements = document.documentElement,
-	body = document.body,//Передаем в переменную body
-	links = document.links;//Получаем все якорные ссылки на странице
+	function animate(draw, duration) {
+		let start = performance.now();
 
-//Функция опредления нажатой ссылки и расчета перемещения
-function calcScroll() {
+		requestAnimationFrame(function animate(time) {
+			let timePassed = time - start;
 
-//Перебор циклом все ссылок и определение той, на которой был сделан клик
-  for (let i = 0; i < links.length; i++) {
-    links[i].onclick = function(event = window.event) {
-    	// event = event || window.event;//Кросс-браузерность
-      //Определение и округление текущего расстояния от верха документа
-      let scrollTop = Math.round(body.scrollTop || elements.scrollTop);
-      if (this.hash !== '') {
-//Предотвращение действия браузера по дефолту при отсутвии атрибута hash у элемента
-        event.preventDefault();
-//Получение элемента, к которому ведет якорь нажатой ссылки
-        let targetElement = document.getElementById(this.hash.substring(1)),
-//Задел в 80px, чтобы при прокрутке меню не закрывало заголовок секции
-			targetElementTop = -80;
-//Вычисление через цикл расстояния от верха до элемента, к которому ведет нажатая ссылка
-        while (targetElement.offsetParent) {
-          targetElementTop += targetElement.offsetTop;
-          targetElement = targetElement.offsetParent;
-        }
-        //Получение округленного значения расположения элемента
-        targetElementTop = Math.round(targetElementTop);
-/* Функция запуска плавного перемещения (содержит аргументы: текущее растояние от верха
-документа, расстояние от верха документа к контентному блоку, к которому ведет нажатая 
-ссылка и сам контентный блок) */
-		if (document.body.style.overflow !== 'hidden') {//Предотвращает прокрутку при открытом модальном окне
-			clearInterval(move);
-			smoothScroll(scrollTop, targetElementTop, this.hash);
-		}
-      }
-    };
-  }
-};
-calcScroll();
+				if (timePassed > duration) {
+					timePassed = duration;
+				}
 
-let timeInterval = 1, //Задаем временной интервал в 1 миллисекунду
-		prevScrollTop,
-		speed,
-		move;
-//Функция плавной прокрутки
-function smoothScroll(from, to, hash) {
-/* Если элемент (конечная точка движения) расположен ниже текущей точки экрана,
-то scroll ведется с верху вниз (положительное значение), если наоборот, то снизу
-вверх (отрицательное значение) */
-	if (to > from) {
-		speed = 10;
-	} else {
-		speed = -10;
-	}
-//Установка интервала движения
-	move = setInterval(function() {
-//Получение и округение текущей позиции экрана
-    scrollTop = Math.round(body.scrollTop || elements.scrollTop);
-//Условия прекращения или продолжения движения
-    if (prevScrollTop === scrollTop || (to > from && scrollTop >= to) || (to < from && scrollTop <= to)) {
-    	clearInterval(move);
-//Добавление атрибута hash в url после прокрутки (добавляется к адресной строке в браузере)
-      history.pushState(history.state, document.title, location.href.replace(/#.*$/g, '') + hash);
-    } else {
-      body.scrollTop += speed;
-      elements.scrollTop += speed;
-/* Передача текущей позиции экрана в переменную, которая при последующих перемещениях
-будет играть роль места хранения последней позиции экрана */
-      prevScrollTop = scrollTop;
-    }
-  }, timeInterval);//Передача ранее установленного интервала перемещения
-}
+				draw(timePassed);
+
+				if (timePassed < duration) {
+					requestAnimationFrame(animate);
+				}
+		});
+	};
+
+	let navigation = document.getElementsByTagName('nav')[0];
+
+		navigation.addEventListener('click', function(event) {
+			event.preventDefault();
+
+			animate(function(timePassed) {
+				let target = event.target,
+					section = document.getElementById(target.getAttribute('href').slice(1));
+
+					window.scrollBy(0, section.getBoundingClientRect().top / 20 - 3);
+			}, 1200);
+
+		});
 
 /* ОПРЕДЕЛЕНИЕ ТИПА БРАУЗЕРА */
 
